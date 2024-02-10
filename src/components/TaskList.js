6625e477fac7470cb4efbd3a7d4ae9871be65b51
Mohamed-art-lab <mohamed.farah@student.moringaschool.com>
2024-02-10 14:34:43 +0300

    Completed Assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks,handleDelete}) {
+
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+ <div className="tasks">
+  {/* display a list of tasks using Task component */}
+  {tasks.map((task) => (
+    <Task text={task.text} category={task.category} key={task.text} onRemove = {handleDelete}/>
+  ))}</div>
   );
 }
 
